1b4692aa355a3020ae5ecd54d8fe4ece1e5f4bda
David Solorio <delosolorio@gmail.com>
2026-03-25 19:09:27 -0400

    added remove task function

--- todo_list.py
+++ todo_list.py
@@ -19,3 +19,18 @@ def view_tasks():
         print("Current tasks: ")
         for i, task in enumerate(tasks, 1): 
             print(f"{i} - {task}")
+
+def remove_task():
+    if not tasks: 
+        return print("No tasks to remove.")
+    view_tasks()
+    try:
+            task_number = int(input("Enter the number of the task you want to remove: "))
+            removed_task = tasks.pop(task_number - 1)
+            print(f"task '{removed_task}' was removed from the list.")
+    except ValueError:
+            print("Invalid input. Please enter a number.")
+            return
+    except IndexError:
+            print("Invalid task number. Please try again.")
+            return
