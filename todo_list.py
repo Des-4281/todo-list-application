3d925ae93fec2782fce422c2e6710cae751917ca
David Solorio <delosolorio@gmail.com>
2026-03-24 22:19:49 -0400

    created view tasks function

--- todo_list.py
+++ todo_list.py
@@ -11,3 +11,11 @@ def add_task():
     task = input("Enter the tasks you would like to add: ")
     tasks.append(task)
     print(f"task '{task}' added to the list.")
+
+def view_tasks():
+    if not tasks:
+        print("No current tasks")
+    else: 
+        print("Current tasks: ")
+        for i, task in enumerate(tasks, 1): 
+            print(f"{i} - {task}")
