cf9d2a80873c181dd88f16db46c6484190ec29bc
David Solorio <delosolorio@gmail.com>
2026-03-24 22:19:12 -0400

    Created add task function

--- todo_list.py
+++ todo_list.py
@@ -6,3 +6,8 @@ def display_menu():
     print("2. View tasks")
     print("3. Remove a task")
     print("4. Exit")
+
+def add_task():
+    task = input("Enter the tasks you would like to add: ")
+    tasks.append(task)
+    print(f"task '{task}' added to the list.")
